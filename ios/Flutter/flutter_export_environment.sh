bac076b86d6b65fc92223595dc316c678dd988d0
yaniestudio <yaniestudio22@gmail.com>
2022-10-24 01:06:00 -0400

    clase11 patron bloc validacion

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=c:\scr\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\REORGANIZANDO\NOVELAS FINALIZADAS\ESTUDIO GUARDAR BIIEN\001 flutter-ios-android-fernando-herrera\11 Validación de formularios - Patrón Bloc\208 flutter-form-validation-1.0.0\flutter-form-validation-1.0.0"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
